44d00eb92b9a75ef39db28c91cd485b6dff97c70
josecc2018 <42903992+josecc2018@users.noreply.github.com>
2020-11-07 15:27:25 -0800

    Main + Command.cpp + Command.h

---
+++ command.cpp
@@ -0,0 +1,56 @@
+#include "Command.h"
+
+Command::Command() {
+	commandWord = "";
+	secondWord = "";
+	commands = { "GO", "QUIT", "HELP", "USE", "INVENTORY", "EQUIP", "ATTACK", "DEFEND" };
+}
+
+
+Command::Command(string commandWord) {
+	this->commandWord = commandWord;
+	secondWord = "";
+	commands = { "GO", "QUIT", "HELP", "USE", "INVENTORY", "EQUIP", "ATTACK", "DEFEND" };
+}
+
+Command::Command(string commandWord, string secondWord) {
+	this->commandWord = commandWord;
+	this->secondWord = secondWord;
+	commands = { "GO", "QUIT", "HELP", "USE", "INVENTORY", "EQUIP", "ATTACK", "DEFEND" };
+}
+
+bool Command::validCommand() {
+	for (int i = 0; i < commands.size(); i++) {
+		
+		string temp = "";
+		for (int j = 0; j < commands[i].size(); j++) {
+			temp += tolower(commands[i].at(j));
+		}
+		if (commandWord == temp || commandWord == commands[i])
+			return true;
+	}
+	return false;
+}
+
+
+void Command::showAll() {
+	for (int i = 0; i < commands.size(); i++) {
+		cout << commands[i] << " ";
+	}
+	cout << endl;
+}
+
+const string Command::getCommandWord(){
+	for (int i = 0; i < commandWord.size(); i++) {
+		commandWord[i] = toupper(commandWord[i]);
+	}
+	return commandWord;
+}
+
+bool Command::hasSecondWord() {
+	return secondWord != "";
+}
+
+string Command::getSecondWord() {
+	return secondWord;
+}
---
+++ main.cpp
@@ -0,0 +1,1 @@
+	corona = new Character("Minotaur", 1000, 40, 3, 0, 70, 0, 10, 5, 6, 0, 12, true);
